30ba89cb7fe871e438634198eedaac04729c7976
Oliver Steele <steele@osteele.com>
2018-01-08 11:08:52 -0500

    Disable Sphinx warnings for non-local images

--- docs/conf.py
+++ docs/conf.py
@@ -82,6 +82,8 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+suppress_warnings = ['image.nonlocal_uri']
+
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
